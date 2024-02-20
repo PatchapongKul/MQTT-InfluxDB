c6a4f3b37accbca21a089db6e4947459b7e831e5
PatchapongKul <patchapong.kul@gmail.com>
2024-02-20 15:04:24 +0700

    add smart sensor sending temperature data every second to MQTT server

---
+++ smart_sensor.py
@@ -0,0 +1,28 @@
+"""
+ 
+MQTT Smart temperature Sensor
+ 
+"""
+ 
+import time 
+import paho.mqtt.client as mqtt
+from faker import Faker
+ 
+# connect to the MQTT broker
+ 
+MQTT_BROKER_URL = "mqtt.eclipseprojects.io"
+MQTT_PUBLISH_TOPIC = "temperature"
+ 
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.connect(MQTT_BROKER_URL)
+ 
+# Init Faker, our fake data provider
+fake = Faker()
+ 
+# Infinite loop of fake data sent to the broker
+while True:
+ 
+    temperature = fake.random_int(min=0, max=30)
+    mqttc.publish(MQTT_PUBLISH_TOPIC, temperature)
+    print(f"Published new temperature measurement: {temperature}")
+    time.sleep(1)
